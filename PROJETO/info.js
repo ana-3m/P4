f9b6b3d3f215d22e2c0b1f943d3c1458b59627e8
anacatarinapalmeida <uc2022222845@student.uc.pt>
2025-05-11 15:19:03 +0100

    js para botão info

---
+++ PROJETO/info.js
@@ -0,0 +1,33 @@
+let selectedFilter = null;
+
+// informacao para cada filtro
+const filterInfo = {
+  tritanopia: "A tritanopia é uma forma rara de daltonismo, onde as pessoas têm dificuldade em distinguir cores no espectro azul-amarelo. É causada pela ausência ou disfunção dos cones do olho responsáveis por perceber o azul. Podem confundir tons de azul com verde, e amarelo com roxo."
+  /*
+  protanopia: "Protanopia: deficiência na percepção do vermelho.",
+  deuteranopia: "Deuteranopia: deficiência na percepção do verde.",
+  acromatopsia: "Acromatopsia: ausência total de percepção de cores.",
+  estrabismoVertical: "Estrabismo vertical (III par): desalinhamento dos olhos no eixo vertical.",
+  estrabismoHorizontal: "Estrabismo horizontal (IV par): desalinhamento lateral.",
+  estrabismoDiagonal: "Estrabismo diagonal (VI par): desalinhamento em diagonal.",
+  nistagmo: "Nistagmo: movimentos involuntários e rápidos dos olhos.",
+  */
+};
+
+// detecta item clicado
+document.querySelectorAll('#menu .word').forEach(item => {
+    item.addEventListener('click', () => {
+      selectedFilter = item.getAttribute('data-filter') || item.id;
+    });
+  });
+
+  // clica no botao do icon 
+  document.getElementById('iconInfo').addEventListener('click', () => {
+    if(selectedFilter && filterInfo[selectedFilter]) {
+        document.getElementById('popupContent').innerText = filterInfo[selectedFilter];
+        document.getElementById('popupInfo').style.display = 'block';
+    }
+  })
+
+
+  // STYLE AINDA NÃO ESTÁ FEITO
